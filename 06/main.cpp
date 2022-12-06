c49a73d3a2a8191f509e21437eee3e842efea0c6
Tomáš Krchňák <krchnak.tomi@gmail.com>
2022-12-06 12:15:45 +0100

    Make it simpler

--- 06/main.cpp
+++ 06/main.cpp
@@ -7,43 +7,34 @@
 #include <string>
 
 
-template< std::size_t count >
-bool check_unique( const std::array< char, count > &window )
+bool check_unique( std::string_view window )
 {
     std::set< char > unique;
     auto pred = [ & ]( char c ) { return unique.insert( c ).second; };
     return std::all_of( window.begin(), window.end(), pred );
 }
 
-template< std::size_t count >
-int move()
+int move( std::size_t count )
 {
     std::ifstream input( "input" );
     std::string line;
     std::getline( input, line );
 
-    std::array< char, count > window;
-    for ( std::size_t i = 0; i < count - 1; ++i )
-        window[ i ] = line[ i ];
-
     for ( std::size_t i = count - 1; i < line.size(); ++i )
-    {
-        window[ i % count ] = line[ i ];
-        if ( check_unique( window ) )
+        if ( check_unique( { line.data() + i - count + 1, count } ) )
             return i + 1;
-    }
 
     assert( false );
 }
 
 void part_1()
 {
-    std::cout << move< 4 >() << std::endl;
+    std::cout << move( 4 ) << std::endl;
 }
 
 void part_2()
 {
-    std::cout << move< 14 >() << std::endl;
+    std::cout << move( 14 ) << std::endl;
 }
 
 int main()
